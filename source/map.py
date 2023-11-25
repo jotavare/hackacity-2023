5e7a876118b42fdcf1d328b44e151dc88139c196
Mário Henriques <111349815+maricard18@users.noreply.github.com>
2023-11-25 23:00:00 +0000

    feat: finish the work produced during the event

--- source/map.py
+++ source/map.py
@@ -1,15 +1,36 @@
 import folium
 import pandas as pd
+import random
 
-df = pd.read_csv('../data/noise_levels_data.csv')
+# Read the CSV file
+df = pd.read_csv('../data/roadConstrains.csv')
 
-# Create a folium map centered at the mean of the coordinates
-map_center = [df['latitude'].mean(), df['longitude'].mean()]
-mymap = folium.Map(location=map_center, zoom_start=10)
+# Convert latitude and longitude to numeric values
+df['latitude'] = pd.to_numeric(df['latitude'], errors='coerce')
+df['longitude'] = pd.to_numeric(df['longitude'], errors='coerce')
 
-# Add markers for each point in the DataFrame
-for index, row in df.iterrows():
-    folium.Marker(location=[row['latitude'], row['longitude']], popup=row['name']).add_to(mymap)
+# Drop rows with missing latitude or longitude values
+df = df.dropna(subset=['latitude', 'longitude'])
 
-# Save the map as an HTML file
-mymap.save('map_with_points.html')
+# Check if there are still rows in the DataFrame
+if not df.empty:
+    # Create a folium map centered at the mean of the coordinates
+    map_center = [df['latitude'].mean(), df['longitude'].mean()]
+    mymap = folium.Map(location=map_center, zoom_start=13.2)
+
+    # Add markers for each event in the DataFrame with random colors
+    for index, row in df.iterrows():
+        # Generate a random color (orange, green, or red)
+        color = random.choice(['orange', 'green', 'red'])
+
+        # Create an Icon with the specified color
+        icon = folium.Icon(color=color)
+
+        # Add marker to the map using the Icon
+        folium.Marker(location=[row['latitude'], row['longitude']], popup=row['subCategory'], icon=icon).add_to(mymap)
+
+    # Save the map as an HTML file
+    mymap.save('map.html')
+    print("Map saved to map.html")
+else:
+    print("No valid coordinates found in the CSV.")
